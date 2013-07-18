321910b41d8ff1988a1dcbe213ccdddd3e73fb75
Marco Zunino <eng.marco.zunino@gmail.com>
2013-07-18 22:56:17 +0200

    Reading html content from index.html file

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
-  response.send('Hello Bitstarter!!');
+    // Let´s read the content from file index.html
+    var indexBuf = fs.readFileSync('index.html');
+    response.send(indexBuf.toString());
 });
 
 var port = process.env.PORT || 5000;
